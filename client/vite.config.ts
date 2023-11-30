dd5a6e31f40db280fa98004f3337d062937b32a5
florrync1808 <112406006+florrync1808@users.noreply.github.com>
2023-11-30 17:59:10 +0000

    base dir

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/client/',
+  base: '/',
   build: {
     outDir: '../build',
   },
